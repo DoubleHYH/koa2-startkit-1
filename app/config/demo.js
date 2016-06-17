3d7a1219d84e4b6d8e87065e0319680cc288cb63
yinz <yinzse@gmail.com>
2016-06-17 18:50:06 +0800

    fix

---
+++ app/config/demo.js
@@ -0,0 +1,15 @@
+// 数据库 配置
+exports.db = {
+  username: null,
+  password: null,
+  database: 'koa',
+  host: '127.0.0.1',
+  dialect: 'postgres'
+}
+
+// 极验证 配置
+exports.geetest = {
+  privateKey: '',
+  publicKey: ''
+}
+
